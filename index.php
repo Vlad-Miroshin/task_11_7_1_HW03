ebd58f0b23d8bb0697ddf3e88ee7a63855061a02
Vlad Miroshin <ppvlad@mail.ru>
2024-07-09 20:20:35 +0300

    feat (index.php) realize foreach

--- index.php
+++ index.php
@@ -13,12 +13,93 @@
     <header>
         <h1>Задание 11.7.1 — Таблицы истинности и сравнения (HW-03)</h1>
     </header>
-    <?php 
-        echo 'Welcome to PHP and enjoy!';
-    ?>
 
     <main>
-        <div class="card-list">
+        <div class="databox">
+
+            <h2>Задание 1. Таблица истинности PHP</h2>
+            <?php 
+                $ab = [
+                    [0, 0],
+                    [0, 1],
+                    [1, 0],
+                    [1, 1]
+                ];
+            ?>
+            <table class="content-table">
+                <thead>
+                    <tr>
+                        <th>A</th>
+                        <th>B</th>
+                        <th>! A</th>
+                        <th>A || B</th>
+                        <th>A && B</th>
+                        <th>A xor B</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php foreach($ab as $item): 
+                        $a = $item[0];
+                        $b = $item[1];
+                    ?>
+                    <tr>
+                        <td><?= $a; ?></td>
+                        <td><?= $b; ?></td>
+                        <td><?= (int)!$a; ?></td>
+                        <td><?= $a | $b; ?></td>
+                        <td><?= $a & $b; ?></td>
+                        <td><?= $a ^ $b; ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+
+            <h2>Задание 2. Таблица сравнения</h2>
+            <?php 
+                $ab = [
+                    [true, "true"],
+                    [false, "false"],
+                    [1, "1"],
+                    [0, "0"],
+                    [-1, "-1"],
+                    ["1", '"1"'],
+                    [null, "null"],
+                    ["php", '"php"'],
+                ];
+            ?>
+            <table class="content-table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <?php foreach($ab as $item): 
+                            $value = $item[0];
+                            $caption = $item[1];
+                        ?>
+                        <th><?= $caption; ?></th>
+                        <?php endforeach; ?>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php foreach($ab as $item): 
+                            $value = $item[0];
+                            $caption = $item[1];
+                    ?>
+                    <tr>
+                        <td class="first-column"><?= $caption; ?></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
 
         </div>
 
